9eab3959d0ca157992debc439cee008e326bf422
JahanzebR <jahanzebtrehman@gmail.com>
2020-11-11 20:47:24 +0400

    added initial libraries, dataset pending

---
+++ textclassification.py
@@ -0,0 +1,13 @@
+import sys
+import nltk
+import sklearn
+import pandas as pd
+import numpy as np
+
+print('Python: {}'.format(sys.version))
+print('NLTK: {}'.format(nltk.__version__))
+print('Scikit-learn: {}'.format(sklearn.__version__))
+print('Pandas: {}'.format(pandas.__version__))
+print('NumPy: {}'.format(numpy.__version__))
+
+# Load the dataset
